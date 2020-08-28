34dd82bf56acd736c16dfb1577bb7bedf763cf1b
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-28 15:02:53 -0700

    Add function to calculate the total cost of the entire order and display it when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -32,14 +32,30 @@ function showShoppingCart(order) {
   $("#myCartItems").empty();
   $("#myCart").show();
   order.items.forEach(function(item) {
-    $("#myCartItems").append("<li> 1 " + item.size + " pizza with " + item.toppings.join(", ") + ".</li>");
+    if (item.toppings.length == 1) {
+      $("#myCartItems").append("<li> 1 " + item.size + " pizza with " + item.toppings.join(", ") + "  -  $" + item.price + "</li>");
+    }
+    if (item.toppings.length == 0) {
+      $("#myCartItems").append("<li> 1 " + item.size + " pizza with no toppings.  -  $" + item.price + "</li>");
+    }
+    if (item.toppings.length > 1) {
+      $("#myCartItems").append("<li> 1 " + item.size + " pizza with " + item.toppings.join(", ") + ".   -  $" + item.price + "</li>");
+    }
   });
+}
+function calcOrderTotal(order) {
+  let total = 0;
+  order.items.forEach(function(item) {
+    total += item.price
+  })
+  $("#total").text(total);
 }
 //USER INTERFACE---------------------------------------------------
 let order = new Order();
 $(document).ready(function() {
   $("form#pizzaType").submit(function(event) {
     event.preventDefault();
+    $("#myCart").hide();
     $(".viewCart").show();
 
     const size = $("#size").val();
@@ -54,5 +70,6 @@ $(document).ready(function() {
   })
   $("#viewCartBtn").click(function() {
     showShoppingCart(order);
+    calcOrderTotal(order);
   })
 })
