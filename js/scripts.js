cb6f2b795c42eb0296296a334fc923bbd555239b
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-28 09:06:28 -0700

    pass first test to create a pizza object using a constructor

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings
+  //this.price = calcPrice();
+}
+
+let pizza = new Pizza("small", toppings = ["cheese", "pepperoni"]);
+console.log(pizza);
